d2baca3557c48921013130bad56a119ff33fd573
mlab817 <mlab817@gmail.com>
2019-11-24 21:59:52 +0800

    added collection-item styling

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import CustomButton from '../../components/custom-button/custom-button.component';
+
 import './collection-item.styles.scss';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
@@ -7,11 +9,13 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
     <div 
       className='image'
       style={{backgroundImage: `url(${imageUrl})`}}
-      ></div>
+      >
+    </div>
     <div className='collection-footer'>
       <span className='name'>{ name }</span>
       <span className='price'>{price}</span>
     </div>
+    <CustomButton inverted>ADD TO CART</CustomButton>
   </div>
 )
 
