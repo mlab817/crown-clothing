8c31cb3ee14f2fb59528783ebb80f772a0af81c9
mlab817 <mlab817@gmail.com>
2019-11-24 20:23:49 +0800

    fixed unsecure firebase config

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,16 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-  apiKey: "AIzaSyBDVGcJhu4kn6GM8Dne2bznrGO_iMoFc_Q",
-  authDomain: "crwn-clothing-dd9ac.firebaseapp.com",
-  databaseURL: "https://crwn-clothing-dd9ac.firebaseio.com",
-  projectId: "crwn-clothing-dd9ac",
-  storageBucket: "crwn-clothing-dd9ac.appspot.com",
-  messagingSenderId: "496953153367",
-  appId: "1:496953153367:web:9bbfd4b45b474b2cb75012",
-  measurementId: "G-P7PQ9YFVR1"
-};
+import { config } from './firebase.config';
 
 export const createUserProfileDocument = async(userAuth, additionalData) => {
   if (!userAuth) return;
