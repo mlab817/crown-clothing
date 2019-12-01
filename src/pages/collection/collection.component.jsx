2db682b69f5d43aa726912c673685be2dada06bc
mlab817 <mlab817@gmail.com>
2019-12-01 22:41:39 +0800

    introduced container pattern

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -26,6 +26,6 @@ const CollectionPage = ({ collection }) =>
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state)
-})
+});
 
 export default connect(mapStateToProps)(CollectionPage);
